2eaa40e0cd92828e40c4df2eb1c8e37ec0562291
Chris Seidl <christopheraseidl@gmail.com>
2025-05-19 14:36:40 +0200

    test(Enums): add tests for OperationScope and OperationType enums

---
+++ tests/Enums/OperationScopeTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace christopheraseidl\HasUploads\Tests\Enums;
+
+use christopheraseidl\HasUploads\Enums\OperationScope;
+
+/**
+ * Tests OperationScope enum structure.
+ *
+ * @covers \christopheraseidl\HasUploads\Enums\OperationScope
+ */
+beforeEach(function () {
+    $this->enum = new \ReflectionEnum(OperationScope::class);
+});
+
+it('is an enum', function () {
+    expect($this->enum->isEnum())->toBeTrue();
+});
+
+it('is backed by a string', function () {
+    $backer = $this->enum->getBackingType();
+
+    expect($backer->getName())->toBe('string');
+});
+
+it('has exactly three cases', function () {
+    expect(count($this->enum->getCases()))->toBe(3);
+});
+
+it('has the correct cases with the correct values', function (string $case) {
+    $scopeCase = constant("christopheraseidl\\HasUploads\\Enums\\OperationScope::$case");
+
+    expect($scopeCase->name)->toBe($case)
+        ->and($scopeCase->value)->toBe(mb_strtolower($case));
+})->with([
+    ['Batch'],
+    ['File'],
+    ['Directory'],
+]);
+
+it('throws an exception for an invalid value', function () {
+    $scope = constant('christopheraseidl\\HasUploads\\Enums\\OperationScope::Nonexistent');
+})->throws(\Error::class, 'Undefined constant christopheraseidl\HasUploads\Enums\OperationScope::Nonexistent');
---
+++ tests/Enums/OperationTypeTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace christopheraseidl\HasUploads\Tests\Enums;
+
+use christopheraseidl\HasUploads\Enums\OperationType;
+
+/**
+ * Tests OperationType enum structure.
+ *
+ * @covers \christopheraseidl\HasUploads\Enums\OperationType
+ */
+beforeEach(function () {
+    $this->enum = new \ReflectionEnum(OperationType::class);
+});
+
+it('is an enum', function () {
+    expect($this->enum->isEnum())->toBeTrue();
+});
+
+it('is backed by a string', function () {
+    $backer = $this->enum->getBackingType();
+
+    expect($backer->getName())->toBe('string');
+});
+
+it('has exactly four cases', function () {
+    expect(count($this->enum->getCases()))->toBe(4);
+});
+
+it('has the correct cases with the correct values', function (string $case) {
+    $scopeCase = constant("christopheraseidl\\HasUploads\\Enums\\OperationType::$case");
+
+    expect($scopeCase->name)->toBe($case)
+        ->and($scopeCase->value)->toBe(mb_strtolower($case));
+})->with([
+    ['Clean'],
+    ['Delete'],
+    ['Move'],
+    ['Update'],
+]);
+
+it('throws an exception for an invalid value', function () {
+    $scope = constant('christopheraseidl\\HasUploads\\Enums\\OperationType::Nonexistent');
+})->throws(\Error::class, 'Undefined constant christopheraseidl\HasUploads\Enums\OperationType::Nonexistent');
