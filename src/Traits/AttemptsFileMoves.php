d85b07425f4078ae6524011113d425b49f8e82ad
Chris Seidl <christopheraseidl@gmail.com>
2025-05-26 20:55:31 +0200

    test: add test for AttemptsFileMoves trait

--- src/Traits/AttemptsFileMoves.php
+++ src/Traits/AttemptsFileMoves.php
@@ -2,23 +2,21 @@
 
 namespace christopheraseidl\HasUploads\Traits;
 
-use Exception;
 use Illuminate\Support\Facades\Storage;
 
 trait AttemptsFileMoves
 {
-    public function attemptMove(string $disk, string $oldPath, string $newDir): string
+    public function attemptMove(string $disk, string $oldPath, string $newDir, int $maxAttempts = 3): string
     {
         $newPath = "{$newDir}/".pathinfo($oldPath, PATHINFO_BASENAME);
         $attempts = 0;
-        $maxAttempts = 3;
 
         while ($attempts < $maxAttempts) {
             try {
                 Storage::disk($disk)->move($oldPath, $newPath);
 
                 return $newPath;
-            } catch (Exception $e) {
+            } catch (\Exception $e) {
                 $attempts++;
                 if ($attempts === $maxAttempts) {
                     throw $e;
@@ -27,6 +25,6 @@ trait AttemptsFileMoves
             }
         }
 
-        throw new Exception("Failed to move file after {$maxAttempts} attempts.");
+        throw new \Exception("Failed to move file after {$maxAttempts} attempts.");
     }
 }
